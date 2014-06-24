9a8293dfa23fe28c93796e6fa834544f92254d67
Cleave <cleavetvtube@gmail.com>
2014-06-24 19:55:26 -0400

    public getter for SceneView.SceneObjects

--- Assets/CleaveFramework/Scene/SceneView.cs
+++ Assets/CleaveFramework/Scene/SceneView.cs
@@ -1,24 +1,11 @@
-﻿/* Copyright 2014 Glen/CleaveTV
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License. */
-using CleaveFramework.Core;
+﻿using CleaveFramework.Core;
 using UnityEngine;
 
 namespace CleaveFramework.Scene
 {
     abstract public class SceneView : View
     {
-        protected SceneObjectData SceneObjects { get; private set; }
+        public SceneObjectData SceneObjects { get; private set; }
 
         virtual public void Start()
         {
@@ -32,7 +19,8 @@ namespace CleaveFramework.Scene
 
         virtual public void Update()
         {
-            SceneObjects.Update(Time.deltaTime);
+            if(SceneObjects != null)
+                SceneObjects.Update(Time.deltaTime);
         }
 
         void OnDestroy()
